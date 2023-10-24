98972ec59de62aa38d6d3073bc9dc23f3abdcc4e
Kiwizacxs <brian.deleon2016@outlook.com>
2023-10-24 20:17:47 +0000

    console

--- console.py
+++ console.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
+"""module that that contains the entry point of the command interpreter"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """class the comand interpreter"""
     prompt = "(hbnb) "
 
     def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
-
-    def help_quit(self):
-        print("Quit command to exit the program\n")
 
     def do_EOF(self, line):
+        """EOF command to exit the program"""
         return True
-    
-    def do_help(self, line):
-        if line == "":
-            print("\nDocumented commands (type help <topic>): \n\
-========================================\n\
-EOF  help  quit\n")
-        else:
-            super().do_help(line)
-        return False
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
