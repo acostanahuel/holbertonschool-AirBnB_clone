df0223193fb1511315517ac069759be899edeb8d
Kiwizacxs <brian.deleon2016@outlokk.com>
2023-10-26 22:50:39 +0000

    arreglarrrr

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """Module that contain the entry point of the command interpreter"""
 
 import cmd
-
+from models.base_model import BaseModel as B
 
 class HBNBCommand(cmd.Cmd):
 
@@ -20,6 +20,18 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) \
+and prints the id
+"""
+        if line == "":
+            print("** class name missing **")
+        args = line.split()
+        with open("file2.json", "a") as o:
+            o.write(B(args[0]))
+            o.write("\n")
+        return False
 
 
 if __name__ == "__main__":
