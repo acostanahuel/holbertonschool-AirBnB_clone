1f8f996b3a99f2c8a0958f9060d4c3d0310a552e
Kiwizacxs <brian.deleon2016@outlook.com>
2023-10-28 21:32:00 +0000

    all

--- console.py
+++ console.py
@@ -8,6 +8,8 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
+    classes = ["BaseModel","Amenity","City","Place","Review","State",
+        "User"]
 
     def do_quit(self, line):
         """Quit command to exit the program.
@@ -38,13 +40,12 @@ and prints the id.
     
     def do_show(self, line):
         """Prints the string representation of an instance based on the \
-class name and id."""
-        classes = ["BaseModel","Amenity","City","Place","Review","State",
-            "User"]
+class name and id.
+"""
         args = line.split()
         if args == []:
             print("** class name missing **")
-        elif args[0] not in classes:
+        elif args[0] not in self.classes:
                 print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
@@ -57,6 +58,43 @@ class name and id."""
                 print("** no instance found **")
         return False
 
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id (save the \
+change into the JSON file).
+"""
+        args = line.split()
+        if args == []:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+                print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            search = f"{args[0]}.{args[1]}"
+            inst = storage.all()
+            if search in inst:
+                del storage.all()[search]
+                storage.save()
+            else:
+                print("** no instance found **")
+        return False
+
+    def do_all(self, line):
+        """Prints all string representation of all instances based or not on \
+the class name.
+"""
+        args = line.split()
+        if len(args) == 0:
+            print(storage.all())
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            all = storage.all()
+            a_list = []
+            for x, inst in all.items():
+                if x.split(".")[0] == args[0]:
+                    a_list.append(str(inst))
+            print(a_list)
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
